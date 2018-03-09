fbc68d556a161dfc840272ca9362b47d4c47740c
Joris Palings <joris@palings.be>
2018-03-09 16:31:00 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Device } from '@ionic-native/device';
+import { Geolocation } from '@ionic-native/geolocation';
+import { DeviceOrientation } from '@ionic-native/device-orientation';
 
 @NgModule({
   declarations: [
@@ -34,6 +37,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
+    Device,
+    Geolocation,
+    DeviceOrientation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Platform, Slides } from 'ionic-angular';
+import { Device } from '@ionic-native/device';
+import { Geolocation } from '@ionic-native/geolocation';
+import { DeviceOrientation } from '@ionic-native/device-orientation';
+import { $WebSocket } from 'angular2-websocket/angular2-websocket'
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,104 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  socket = new $WebSocket('wss://make-it-rain-server.herokuapp.com');
+  // socket = new $WebSocket('ws://localhost:6969');
+  position: any;
+  heading: any;
+  swipedCount = 0;
+  clientsInFOV;
 
+  constructor(public navCtrl: NavController,
+    private platform: Platform,
+    private device: Device,
+    private geolocation: Geolocation,
+    private deviceOrientation: DeviceOrientation) {
+    platform.ready().then(() => {
+      this.socket.send({ type: 'handshake' }).subscribe(
+        (message) => {
+          console.info(message);
+        },
+        (error) => {
+          console.error('Error:', error);
+        },
+        () => {
+          console.info('Handshake sent');
+        }
+      );
+      this.socket.onMessage(message => {
+        const parsedMessage = JSON.parse(message.data);
+        if(parsedMessage.type === 'clients') {
+          const clientsInFOV = parsedMessage.data;
+          console.info('Clients in your field of view:', clientsInFOV.map(client => client.id));
+          this.clientsInFOV = clientsInFOV;
+        }
+      });
+      this.initGeolocation();
+      this.initDeviceOrientation();
+    });
+  }
+
+  initGeolocation() {
+    this.geolocation.watchPosition().subscribe(
+      (position) => {
+        if(position.coords) {
+          this.position = position.coords;
+          this.socket.send({ type: 'position', data: { latitude: position.coords.latitude, longitude: position.coords.longitude } }).subscribe(
+            (message) => {
+              console.info(message);
+            },
+            (error) => {
+              console.error('Error: Failed to send position', error);
+            },
+            () => {
+              console.info('Position sent');
+            }
+          );
+        }
+      },
+      (error) => {
+        console.error('Error: Failed to get current position', error);
+      }
+    );
+  }
+
+  initDeviceOrientation() {
+    this.deviceOrientation.watchHeading().subscribe(
+      (heading) => {
+        this.heading = heading.trueHeading;
+        this.socket.send({ type: 'heading', data: heading.trueHeading }).subscribe(
+          (message) => {
+            console.info(message);
+          },
+          (error) => {
+            console.error('Error: Failed to send heading', error);
+          },
+          () => {
+            console.info('Heading sent');
+          }
+        );
+      },
+      (error) => {
+        console.error('Error: Failed to get current heading', error);
+      }
+    );
+  }
+
+  handleSlideWillChange(event) {
+    this.swipedCount++;
+    if (this.swipedCount > 1) {
+      this.socket.send({ type: 'payment' }).subscribe(
+        (message) => {
+          console.info(message);
+        },
+        (error) => {
+          console.error('Error: Failed to make payment', error);
+        },
+        () => {
+          console.info('Payment made');
+        }
+      );
+    }
   }
 
 }
